bc11aedbe62aa77dc8511de1b3be189522a824db
Ran-x2 <rxr456@case.edu>
2025-11-05 22:33:39 -0500

    update

--- RNAseq/analysis/TPM_cibersort_mixture_prepare.R
+++ RNAseq/analysis/TPM_cibersort_mixture_prepare.R
@@ -5,8 +5,10 @@ devtools::install_github("BaderLab/HumanLiver")
 library(HumanLiver)
 viewHumanLiver()
 library(dplyr)
+library(tibble)
+setwd('C:/Users/16220/Documents/GitHub/gut-liver-model/RNAseq/')
 
-metadata = read.csv('D:/Trapecar/metadata.csv')
+metadata = read.csv('analysis/data/metadata.csv')
 metadata$Study.Design <- sub("-[0-9]+$", "", metadata$Study.Design)
 
 metadata$Donor <- factor(metadata$Donor)
@@ -18,49 +20,66 @@ metadata$Cell.Type.Study.Design = factor(paste0(metadata$Cell.Type,'__',metadata
 metadata$Treatment <- relevel(metadata$Treatment, ref = "NO TREATMENT")
 metadata$Study.Design <- relevel(metadata$Study.Design, ref = "ISOLATION (ISO) PURE")
 
-raw_counts = read.csv('D:/Trapecar/TPM_counts_STAR_RSEM.csv',row.names = 1)
+raw_counts = read.csv('analysis/data/TPM_counts_STAR_RSEM.csv',row.names = 1)
 raw_counts = raw_counts[,metadata$Sample.Name]
 unique_celltypes <- unique(metadata$Cell.Type)
 unique_donor <- unique(metadata$Donor)
 
-choose_id <- function(x) {
-  if (length(x) == 2) {
-    return(x[2])
+ref = read.csv('analysis/features_10x_2020A.tsv',sep = '\t',header = 1)
+lookup = ref[,2]
+names(lookup) = ref[,1]
+
+# Find duplicated values in lookup
+dup_vals <- lookup[duplicated(lookup)]
+# For each duplicated value, append the name (key) to the value with "_"
+for (key in names(dup_vals)) {
+  val <- lookup[key]
+  lookup[key] <- paste(val, key, sep = "_")
+}
+lookup[duplicated(lookup)]
+
+split_names <- strsplit(row.names(raw_counts), "_")
+ensg_id = sapply(split_names, function(x) x[1])
+gene_names = sapply(split_names, function(x) x[2])
+
+choose_id <- function(x, y) {
+  if (length(x) == 2 && x[1] %in% names(y)) {
+    return(y[[x[[1]]]])
+  } else if (length(x) == 2) {
+    return(paste0(c(x[[2]], x[[1]]), collapse = "_"))
   } else {
-    return(x[1])
+    return(x[[1]])
   }
 }
-split_names <- strsplit(row.names(raw_counts), "_")
-string_array <- sapply(split_names, choose_id)
-raw_counts['Gene.ID'] = string_array
-summed_counts <- raw_counts %>%
-  group_by(Gene.ID) %>%
-  summarise(across(everything(), sum, na.rm = TRUE))
-write.csv(summed_counts,'D:/Trapecar/summed_TPM_counts.csv')
+mapped_gene_names <- mapply(choose_id, split_names, MoreArgs = list(y = lookup))
 
+mapped_gene_names[duplicated(mapped_gene_names)]
+row.names(raw_counts) <- mapped_gene_names
+write.csv(raw_counts,'analysis/data/symbol_mappedTPM_counts.csv')
+
+
+for (celltype in unique_celltypes) {
+subset_metadata <- metadata %>%
+  filter(Cell.Type == celltype)
+subset_counts <- raw_counts[, subset_metadata$Sample.Name]
+subset_filename = paste('deconvolution/input_for_cibersortx/',celltype,'_TPM.tsv',sep ='')
+subset_filename = gsub(" ", "_", subset_filename)
+subset_filename = gsub("\\+", 'pos', subset_filename)
+print(subset_filename)
+subset_counts = add_column(subset_counts, GeneSymbol = row.names(subset_counts), .before = 1)
+write.table(subset_counts,subset_filename, sep = "\t",row.names = FALSE,quote=FALSE)
+}
 
 for (donor in unique_donor) {
 for (celltype in unique_celltypes) {
 subset_metadata <- metadata %>%
   filter(Donor == donor, Cell.Type == celltype)
-
-subset_counts <- summed_counts[, c('Gene.ID',subset_metadata$Sample.Name)]
-subset_filename = paste('D:/Trapecar/',donor,celltype,'.tsv',sep ='')
+subset_counts <- raw_counts[, subset_metadata$Sample.Name]
+subset_filename = paste('deconvolution/input_for_cibersortx/by_donor/',donor,'_',celltype,'_TPM.tsv',sep ='')
 subset_filename = gsub(" ", "_", subset_filename)
 subset_filename = gsub("\\+", 'pos', subset_filename)
 print(subset_filename)
+subset_counts = add_column(subset_counts, GeneSymbol = row.names(subset_counts), .before = 1)
 write.table(subset_counts,subset_filename, sep = "\t",row.names = FALSE,quote=FALSE)
 }
 }
-##############################################################
-library(data.table)
-liver_data = read.csv('D:/Trapecar/GSE115469_Data.csv',row.names = 1)
-annotation_liver = read.csv('D:/Trapecar/GSE115469_CellClusterType.txt', sep = '\t')
-colnames(liver_data) ==annotation_liver$CellName
-liver_data = data.frame(liver_data, check.names = FALSE)
-colnames(liver_data) = annotation_liver$CellType
-names(liver_data) <- sub("\\.\\d+$", "", names(liver_data))
-colnames(liver_data)
-liver_data_hep = liver_data[,startsWith(colnames(liver_data),'Hep')]
-write.table(liver_data_hep,'D:/Trapecar/deconvolute/hep_sig.txt', sep = "\t",row.names = TRUE,quote=FALSE)
-###############################################################
